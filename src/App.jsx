cfbcfc78b939ec0228cb89625f85ac44466b21cc
kelani34 <kelanitaiwo34@gmail.com>
2023-02-07 16:19:00 +0100

    fix: router error

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { Home, Signup, Login, Verify, ResetPassword, Connect } from "./pages";
 
 function App() {
